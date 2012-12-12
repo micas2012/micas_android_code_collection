116ab6668788c30e91b6f44d56b31b3fbe016705
micas2012 <yy_cnsn@hotmail.com>
2012-12-12 01:44:48 -0800

    Create Activity02.java
    AsyncTask的规范型很强，能够时时反映更新的情况
    
    它一般有这么几个方法
    
     * onPreExecute(), 该方法将在执行实际的后台操作前被UI 线程调用。可以在该方法中做一些准备工作，如在界面上显示一个进度条，或者一些控件的实例化，这个方法可以不用实现。
           * doInBackground(Params...), 将在onPreExecute 方法执行后马上执行，该方法运行在后台线程中。这里将主要负责执行那些很耗时的后台处理工作。可以调用 publishProgress方法来更新实时的任务进度。该方法是抽象方法，子类必须实现。
          * onProgressUpdate(Progress...),在publishProgress方法被调用后，UI 线程将调用这个方法从而在界面上展示任务的进展情况，例如通过一个进度条进行展示。
          * onPostExecute(Result), 在doInBackground 执行完成后，onPostExecute 方法将被UI 线程调用，后台的计算结果将通过该方法传递到UI 线程，并且在界面上展示给用户.
    
          * onCancelled(),在用户取消线程操作的时候调用。在主线程中调用onCancelled()的时候调用。
    
    为了正确的使用AsyncTask类，以下是几条必须遵守的准则：
    
        1) Task的实例必须在UI 线程中创建
    
        2) execute方法必须在UI 线程中调用
    
        3) 不要手动的调用onPreExecute(), onPostExecute(Result)，doInBackground(Params...), onProgressUpdate(Progress...)这几个方法，需要在UI线程中实例化这个task来调用。
    
        4) 该task只能被执行一次，否则多次调用时将会出现异常

---
+++ Activity02.java
@@ -0,0 +1,116 @@
+package com.android.wei.thread;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class Activity02 extends Activity{
+  
+	/**开始StartAsync按钮**/
+	Button mButton = null;
+	
+	Context mContext = null;
+	
+	//内容显示出来
+	TextView mTextView = null;
+	
+	//Timer 对象
+	Timer mTimer = null;
+	
+	/** timerTask 对象**/
+	TimerTask mTimerTask = null;
+	
+	/**记录TimerId**/
+	int mTimerId =0;
+	/**图片列表**/
+	private List<Bitmap> list;
+	/**图片容器**/
+	private ImageView mImageView;
+	public void onCreate(Bundle s){
+		super.onCreate(s);
+		setContentView(R.layout.main);
+		mContext = this;
+		mButton =(Button) this.findViewById(R.id.button1);
+		mImageView =(ImageView)this.findViewById(R.id.imageview);
+		mTextView =(TextView) this.findViewById(R.id.textView1);
+		mButton.setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				StartAsync();
+			}
+		});
+		
+		
+	}
+	public void StartAsync(){
+		new AsyncTask<Object,Object,Object>(){
+            protected void onPreExecute(){
+            	//首先执行这个方法，它在UI线程中，可以执行一些异步操作
+            	mTextView.setText("开始加载进度");
+            	super.onPreExecute();
+            }
+			@Override
+			protected Object doInBackground(Object... params) {
+				// TODO Auto-generated method stub
+				//异步后台执行，执行完毕可以返回出去一个结果 Object 对象
+				//得到开始加载得时间
+				list = new ArrayList<Bitmap>();
+				for(int i =0;i<100;i++){
+					Bitmap bitmap =ReadBitmap(mContext,R.drawable.icon);					
+					final ByteArrayOutputStream os = new ByteArrayOutputStream();
+					bitmap.compress(Bitmap.CompressFormat.PNG, 100, os);
+					byte[] image = os.toByteArray();
+					Bundle bundle = new Bundle();
+					bundle.putInt("index", i);
+					bundle.putByteArray("image", image);
+					publishProgress(bundle);
+					list.add(bitmap);
+					
+				}
+				
+				return list;
+			}
+			protected void onPostExecute(Object result){
+				//doInBackground 执行之后在这里可以接受到返回结果的对象
+				List<Bitmap> list = (List<Bitmap>) result;
+				mTextView.setText("一共加载了"+list.size()+"张图片");
+			   super.onPostExecute(result);
+			}
+			protected void onProgressUpdate(Object... values){
+				//时时拿到当前的进度更新UI
+				Bundle bundle = (Bundle)values[0];
+				byte[] image = bundle.getByteArray("image");
+				Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
+				int index = bundle.getInt("index");
+				mTextView.setText("当前加载进度"+index);
+				mImageView.setImageBitmap(bitmap);
+				super.onProgressUpdate(values);
+			}
+			
+		}.execute();
+	}
+	public Bitmap ReadBitmap(Context context,int resId){
+		BitmapFactory.Options opt = new BitmapFactory.Options();
+		opt.inPreferredConfig = Bitmap.Config.RGB_565;
+		opt.inPurgeable = true;
+		opt.inInputShareable = true;
+		InputStream is = context.getResources().openRawResource(resId);
+		return BitmapFactory.decodeStream(is, null, opt);
+	}
+	
+}
